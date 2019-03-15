abcc8718bfd0ff0a21470c4c251f7d953bd5a010
SarahArroudj <47893641+SarahArroudj@users.noreply.github.com>
2019-03-15 21:44:47 +0100

    Update main.py

--- Python_Code/main.py
+++ Python_Code/main.py
@@ -1,131 +1,83 @@
 import picamera
-import time 
 from time import sleep
-import MySQLdb as msql
 
-####### SQL configuration ########
-config = {
-    'user': 'user',
-    'host':'192.168.0.69',
-    'passwd':'Motdepasse@123',
-    'db':'test',
-    'port':'3306'
-    }
-    #connect to server
-db = msql.connect(**config)
-
-#def exe_query(db, dataa,newIne):
-    cur = db.cursor()
-result=cur.execute("UPDATE `test_test` SET state = "+dataa+" WHERE ine = "+newIne+"")
-    db.commit()
-    cur.close()
+# Our library
+from text_recognition import *
+from database import *
+from HMI import *
 
 
 
-##########################################################################################    
-#    func    find_ine
-#    param    db             database accessor
-#    param    newIne        Ine to check
+
+
+##########################################################################################
 #
-#    brief    serch database blablablabl
-#
-#    return  0    The student is not in the list
-#    return     1    The student is in the list and validate
+#	
 #
 ##########################################################################################
-def find_ine(db,newIne):
-cur = db.cursor()
-    result=cur.execute("SELECT `ine`  FROM  `test_test` ")
-    db.commit()
-    for row in cur:
-        if row==newIne:
-            res = 1
-            
+def is_student_validated(newIne):
 
-    if res==1:
-        print('etudiant figure dans la liste')
-        upd = cur.execute("UPDATE `test_test` SET `State`=1")
-        return 1
-    else:
-        print('etudiant intrus')
-        return 0
-    cur.close()
-    ##########################################################################################
-# func        print_present
-# param     db         database accessor
-# param     newIne    Ine to check
-#  
-# brief     Check if the student is validated and mark him present
-#
-# return   0    the student isn't validated yet
-# return   1    the student is already validated
-########################################################################################## 
-
-def print_present(db,newIne):
-    cur = db.cursor()
-    verif = cur.execute("SELECT `Nom`  FROM  `test_test`  WHERE state ")
-    for row in cur:
-        if row == 0:
-            return 0
-            else:
-            print ('Letudiant a deja badge')
-            return 1
-    result=cur.execute("SELECT `Nom`  FROM  `test_test`  WHERE state = 1")
-    db.commit()
-    for row in cur:
-        print('Letudiant %s est present',row)
-    cur.close()
-##########################################################################################
-# func        display present
-# param     db         database accessor
-# 
-# brief     print the number of students that are present 
-# 
-########################################################################################## 
-
-def display_present(db):
-    cur = db.cursor()
-    total = cur.execute("SELECT COUNT(State) FROM test_test ")
-    present = cur.execute ("SELECT COUNT(State) FROM test_test WHERE State = 1") 
-    print ('Il y a %d / %d  de presents', present, total)
-    cur.close()
-   
-
-
-
-
-def detect_INE():
-    newIne="okdh";
-    return newIne
+  # Verify if the INE number exists in the database
+  ine_exists = validate_student(db,newIne)
+  
+  # if the student is in the list : Green Led + sound
+  if (ine_exists == 1):
+    print("Student in the list but refused : already badged")
+    #display_result(1)
     
+  elif (ine_exists == 0):
+    print("Student in the list and accepted")
+    #display_result(1) 
+      
+  elif (ine_exists == -1):
+    print("Student in not the list")
+    #display_result(1)   
+
+  
+  
+##########################################################################################
+#
+#	
+#
+########################################################################################## 
 if __name__ == "__main__":
+  
+  print("starting EasyStudentsScan")
+  
+  
+  #init_HMI()
+  
+  
+  
+  
+  #    while 1 :
+  
+  ## Take a picture using the camera
+  camera = picamera.PiCamera()
+  try:
+  camera.start_preview()
+  time.sleep(10)
+  camera.capture('carte_1.jpeg')
+  camera.stop_preview()
+  finally:
+  camera.close()
+  
+  # Extract INE number from the photo
+  newIne = read_picture('carte_1.jpg')
+  print ("Detected Ine : " + newIne)
+  
+  
+  print("test 1")
+  is_student_validated(newIne)
+  print("test 2")
+  is_student_validated(newIne)
 
-    print("starting EasyStudentsScan")
+  
+  
+  
+  # Print the number of student present on the screen
 
-#    while 1 :
- # Take a picture using the camera
-camera = picamera.PiCamera()
-try:
-    camera.start_preview()
-    time.sleep(10)
-    camera.capture('/home/pi/Desktop/Proto204/Examples/imh.jpeg')
-    camera.stop_preview()
-finally:
-    camera.close()
-
-        # Extract INE number from the photo
-    detect_INE()
-
-        # Verify if the INE number exists in the database
-            
-    find_ine(db,newIne)
-        # if the student is in the list : Green Led + sound
-        # else if the student is not in the list : Red led + sound
-    print_present(db,newIne)
-
-        # Print the number of student present on the screen
-    display_present(db)
-
-        # If the button is pressed : database is reseted
-
-        # Wait 20 seconds
+  # If the button is pressed : database is reseted
+  
+  # Wait 20 seconds
+  time.sleep(20)
